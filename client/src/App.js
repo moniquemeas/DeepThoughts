c7db4c1d8d761d9c54649ffdd8d0fcd8e9a2b0fc
Monique <monique.meas@gmail.com>
2022-11-08 02:48:57 -0500

    created queries for all thought and used in homepage

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,28 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>
   );
 }
 
